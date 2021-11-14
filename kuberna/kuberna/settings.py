5f3272aa61a5d5ee6e0e4e907f739807d3c82ab2
obousselham <87040599+obousselham@users.noreply.github.com>
2021-11-14 22:20:27 +0100

    profs init
    prof init

--- kuberna/kuberna/settings.py
+++ kuberna/kuberna/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'professeurs',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'kuberna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kuberna',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- kuberna/professeurs/models.py
+++ kuberna/professeurs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Professeur(models.Model):
+    
+    profId = models.AutoField(primary_key=True)
+    experience = models.IntegerField(null=False, blank=False)
+    orgId = models.IntegerField()
+    fonction = models.CharField(null=False, blank=False, max_length=255)
+    nomme = models.CharField(null=False, blank=False,max_length=5)
+    tempprio = models.CharField(null=False, blank=False,max_length=5)
+    tempor = models.CharField(null=False, blank=False,max_length=5)
+    CAD =models.CharField(null=False, blank=False,max_length=3)
+    CADFrom = models.DateField()
+    CADTo = models.DateField()
+    
+    class Meta():
+        db_table = "professeurs"
+
+    
+
+    
